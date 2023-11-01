de44838dc1ba7d339abd24e31de496ca77475391
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-11-01 21:06:48 +0330

    Change App Bar And Create List

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 2,
-        title: Text('S A A D Coach'),
+      backgroundColor: Colors.grey.shade800,
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return const Row(
+              children: [
+                Text(
+                  'Exercise\nProgram',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 36,
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Container(
+              height: 186,
+              width: double.infinity,
+              margin: const EdgeInsets.only(top: 32),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color: Colors.black54,
+              ),
+              child: Row(),
+            );
+          }
+        },
       ),
     );
   }
