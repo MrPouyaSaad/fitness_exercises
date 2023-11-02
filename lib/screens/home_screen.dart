6905161778554fce4c1c69c411ffdeac3cce48ce
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-11-02 12:17:05 +0330

    Add Category List

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,7 +9,6 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey.shade800,
       body: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
         itemCount: 5,
         itemBuilder: (context, index) {
           if (index == 0) {
@@ -23,12 +23,14 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ],
-            );
+            ).marginSymmetric(horizontal: 24, vertical: 16);
+          } else if (index == 1) {
+            return const CategoryList();
           } else {
             return Container(
               height: 186,
               width: double.infinity,
-              margin: const EdgeInsets.only(top: 32),
+              margin: const EdgeInsets.only(bottom: 32, left: 24, right: 24),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(18),
                 color: Colors.black54,
@@ -36,6 +38,57 @@ class HomeScreen extends StatelessWidget {
               child: Row(),
             );
           }
+        },
+      ),
+    );
+  }
+}
+
+class CategoryList extends StatefulWidget {
+  const CategoryList({super.key});
+
+  @override
+  State<CategoryList> createState() => _CategoryListState();
+}
+
+class _CategoryListState extends State<CategoryList> {
+  int selectedItem = 0;
+  List categories = ['For you', 'Challenges', 'Exercises', 'Supplement'];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      height: 64,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.only(top: 8, bottom: 24),
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedItem = index;
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.only(
+                  left: 24, right: index == categories.length - 1 ? 24 : 0),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: index == selectedItem
+                    ? Colors.white.withOpacity(0.4)
+                    : Colors.white.withOpacity(0.075),
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              child: Text(
+                categories[index],
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          );
         },
       ),
     );
