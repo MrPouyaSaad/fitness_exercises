6e309d1a9daeb4cce1463ae3d1f0fe5632f7117a
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-11-03 00:05:26 +0330

    Add Card Model

---
+++ lib/models/card.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class CardItem {
+  String title;
+  String imagePatch;
+  String caption;
+  CardItem({
+    required this.title,
+    required this.imagePatch,
+    required this.caption,
+  });
+}
