7d700211666f24889bc6c084e82a65c34f523935
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-11-09 23:10:37 +0330

    Create Chest Screen

---
+++ lib/screens/exercise/chest.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChestExercisesScreen extends StatelessWidget {
+  const ChestExercisesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
