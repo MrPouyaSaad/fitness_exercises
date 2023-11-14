28ab871ad2907edd46ea527685f0ff5ff1efc2c5
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-11-14 20:03:36 +0330

    Add Arm exercise list

---
+++ lib/constant/exercise_list.dart
@@ -0,0 +1,23 @@
+import 'package:fitness_exercises/models/card.dart';
+
+List<CardItem> armList = [
+  CardItem(imagePatch: 'arm/1', title: 'Barbell Curl'),
+  CardItem(imagePatch: 'arm/2', title: 'LARRY SCOTT CURL'),
+  CardItem(imagePatch: 'arm/3', title: 'Barbell Spider Curl'),
+  CardItem(imagePatch: 'arm/4', title: 'Barbell e-z Curl'),
+  CardItem(imagePatch: 'arm/5', title: 'arm overhead cable curl'),
+  CardItem(imagePatch: 'arm/6', title: 'one-arm dumbbell concentration curl'),
+  CardItem(imagePatch: 'arm/7', title: 'one-arm dumbbell curl'),
+  CardItem(imagePatch: 'arm/10', title: 'two-arm dumbbell curl'),
+  CardItem(imagePatch: 'arm/8', title: 'one-arm dumbbell LARRY SCOTT CURL'),
+  CardItem(imagePatch: 'arm/15', title: 'two-arm dumbbell LARRY SCOTT CURL'),
+  CardItem(imagePatch: 'arm/12', title: 'Dumbbell hammer curl'),
+  CardItem(imagePatch: 'arm/13', title: 'Dumbbell Seated Hammer Curl'),
+  CardItem(imagePatch: 'arm/16', title: 'Seated Incline Dumbbell Biceps Curl'),
+  CardItem(imagePatch: 'arm/17', title: 'CABLE ROPE CURL'),
+  CardItem(imagePatch: 'arm/18', title: 'SUPINE CABLE BICEPS CURL'),
+  CardItem(imagePatch: 'arm/19', title: 'Cable curl'),
+  CardItem(imagePatch: 'arm/20', title: 'larry scott machine'),
+  CardItem(imagePatch: 'arm/21', title: 'larry scott cable'),
+  CardItem(imagePatch: 'arm/9', title: 'Plate Curls'),
+];
